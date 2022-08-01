ebf36a55636f3c5664613cecbf5f30bdd4a1ee16
roshramos <rosh.ramos30@hotmail.com>
2022-08-01 17:55:51 +1000

    added burger menu for books page

--- books.js
+++ books.js
@@ -1,3 +1,11 @@
+function openMenu() {
+  document.body.classList +=" menu--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu--open')
+}
+
 let books;
 
 async function renderBooks(filter) {
